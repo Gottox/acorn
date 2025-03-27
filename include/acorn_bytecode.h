b86807d5dff153094df8851b24525464b98b8c88
Enno Boland <g@s01.de>
2025-03-27 08:16:30 +0100

    add meson to generate quickjs bytecode

---
+++ include/acorn_bytecode.h
@@ -0,0 +1,10 @@
+#ifndef ACORN_BYTECODE_H
+#define ACORN_BYTECODE_H
+
+#include <stdint.h>
+
+extern const uint32_t acorn_bytecode_size;
+
+extern const uint8_t acorn_bytecode[];
+
+#endif // ACORN_BYTECODE_H
